b8881141cce138a79a25019e9cda9ceef1db061a
pk <pk@ex.com>
2026-03-27 22:36:25 +0530

    added new feature in todo apps

--- doList/src/ToDoList.jsx
+++ doList/src/ToDoList.jsx
@@ -1,60 +1,61 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
-function ToDoList(){
-    const [tasks, setTasks] = useState(["1.Dsa","2.revision","3.React","4.Node","5.web"]);
+function ToDoList() {
+    const [tasks, setTasks] = useState(["1.Dsa", "2.revision", "3.React", "4.Node", "5.web"]);
     const [newTask, setNewTask] = useState("");
-    function handleInputChange(event){
+    function handleInputChange(event) {
         setNewTask(event.target.value);
 
     }
-    function addTask(){
-        if(newTask.trim() !== ""){
-            setTasks(t => [...t,newTask]);
+    function addTask() {
+        if (newTask.trim() !== "") {
+            setTasks(t => [...t, newTask]);
             setNewTask("");
         }
 
     }
-    function deleteTask(index){
+    function deleteTask(index) {
         const updatedTasks = tasks.filter((_, i) => i !== index);
         setTasks(updatedTasks);
 
     }
-    function moveTaskUp(index){
-        if(index>0){
+    function moveTaskUp(index) {
+        if (index > 0) {
             const updatedTasks = [...tasks];
-            [updatedTasks[index], updatedTasks[index-1]] =
-            [updatedTasks[index-1], updatedTasks[index]];
+            [updatedTasks[index], updatedTasks[index - 1]] =
+                [updatedTasks[index - 1], updatedTasks[index]];
             setTasks(updatedTasks);
         }
 
 
     }
-    function moveTaskDown(index){
-        if(index<tasks.length-1){
+    function moveTaskDown(index) {
+        if (index < tasks.length - 1) {
             const updatedTasks = [...tasks];
-            [updatedTasks[index], updatedTasks[index+1]] =
-            [updatedTasks[index+1], updatedTasks[index]];
+            [updatedTasks[index], updatedTasks[index + 1]] =
+                [updatedTasks[index + 1], updatedTasks[index]];
             setTasks(updatedTasks);
         }
 
     }
-    return(
+    return (
         <div className="to-do-list">
-            <h1>To-Do-List Helloxcm</h1>
+            <h1>To-Do-List</h1>
+            <h2>New feature added</h2>
             <div>
-                <input type="text" placeholder='Enter a task...' value={newTask} onChange={handleInputChange}/>
+                <input type="text" placeholder='Enter a task...' value={newTask} onChange={handleInputChange} />
                 <button className="add-button" onClick={addTask}>Add</button>
-    
+
             </div>
             <ol>
                 {tasks.map((task, index) =>
-                <li key={index}>
-                    <span className="text">{task}</span>
-                    <button className="delete-button"
-                    onClick={() => deleteTask(index)}>Delete</button>
-                    <button className="move-button" onClick={()=>moveTaskUp(index)}>Y</button>
-                    <button className="move-button" onClick={()=>moveTaskDown(index)}>x</button>
-                </li>)}
+                    <li key={index}>
+                        <span className="text">{task}</span>
+                        <button className="delete-button"
+                            onClick={() => deleteTask(index)}>Delete</button>
+                        <button className="move-button" onClick={() => moveTaskUp(index)}>Y</button>
+                        <button className="move-button" onClick={() => moveTaskDown(index)}>x</button>
+                    </li>)}
             </ol>
         </div>
     );
